4f9927737fb53b48db0348cd107689fefdb48dd0
ElzaCS <anuejohn@gmail.com>
2021-03-15 13:37:35 +0530

    Add intents for camera, notes, alarm

--- app/src/main/java/com/example/emptyactivityexample/IntentUsageActivity.java
+++ app/src/main/java/com/example/emptyactivityexample/IntentUsageActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
 public class IntentUsageActivity extends AppCompatActivity implements View.OnClickListener {
     final String message = "I am learning intents! ";
+    public static final int CAMERA_REQUEST = 1;
 
     public static final String EXTRA_TEXT2 = "com.example.emptyactivityexample.EXTRA_TEXT2";
 
@@ -37,6 +40,18 @@ public class IntentUsageActivity extends AppCompatActivity implements View.OnCli
         //VIEW INTENTS
         Button btn_viewIntents = findViewById(R.id.btn_viewIntents);
         btn_viewIntents.setOnClickListener(this);
+
+        //CAMERA
+        Button btn_camera = findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(this);
+
+        //NEW NOTE
+        Button btn_note = findViewById(R.id.btn_note);
+        btn_note.setOnClickListener(this);
+
+        //ALARM
+        Button btn_alarm = findViewById(R.id.btn_alarm);
+        btn_alarm.setOnClickListener(this);
 
         //BACK BUTTON
         Button btn_click = findViewById(R.id.btn_back_i);
@@ -67,7 +82,7 @@ public class IntentUsageActivity extends AppCompatActivity implements View.OnCli
 
             //OPEN WEBPAGE
             case R.id.btn_webpage:
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nitt.edu"));
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com"));
                 startActivity(intent);
                 break;
 
@@ -81,6 +96,32 @@ public class IntentUsageActivity extends AppCompatActivity implements View.OnCli
             case R.id.btn_viewIntents:
                 intent = new Intent("com.example.emptyactivityexample.IntentGetActivity");
                 startActivity(intent);
+                break;
+
+            //CAMERA
+            case R.id.btn_camera:
+                //image
+                intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //video
+                //intent= new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivityForResult(intent, CAMERA_REQUEST);
+                break;
+
+            //NEW NOTE
+            case R.id.btn_note:
+                intent = new Intent(Intent.ACTION_SEND)
+                        .putExtra(Intent.EXTRA_TITLE, "subject")
+                        .putExtra(Intent.EXTRA_TEXT, "text");
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, "Create note..."));
+                break;
+
+            //ALARM
+            case R.id.btn_alarm:
+                intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivity(intent);
                 break;
 
             //BACK BUTTON
